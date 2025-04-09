0bd910bb907577b5ec086ba6bbc7c4ed1c0f196b
Mokgadi Phasha <phashamokgadi631@gmail.com>
2025-04-09 21:22:42 +0200

    Implemented admin service manager skeleton

--- src/main/java/com/taxiapi/Service/AdminService/AdminServiceManager.java
+++ src/main/java/com/taxiapi/Service/AdminService/AdminServiceManager.java
@@ -2,17 +2,19 @@ package com.taxiapi.Service.AdminService;
 
 import com.taxiapi.Model.TaxiRoute;
 import com.taxiapi.Repository.TaxiRouteRepository;
-import com.taxiapi.Service.GenericCrudServiceIIII;
+import com.taxiapi.Service.GenericCrudService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AdminServiceManagerIIII extends GenericCrudServiceIIII<TaxiRoute,Long> {
+public class AdminServiceManager extends GenericCrudService<TaxiRoute,Long> {
     private TaxiRouteRepository repository ;
 
-    public AdminServiceManagerIIII(TaxiRouteRepository repository) {
+    public AdminServiceManager(TaxiRouteRepository repository) {
         super(repository);
         this.repository = repository;
     }
+
+
 
     
 
