b7a8eb426061e2401355a54cea00aa388f37df02
Mokgadi Phasha <phashamokgadi631@gmail.com>
2025-04-11 10:22:25 +0200

    modified repository to find the custom db info for routes

--- src/main/java/com/taxiapi/Repository/TaxiRouteRepository.java
+++ src/main/java/com/taxiapi/Repository/TaxiRouteRepository.java
@@ -3,5 +3,9 @@ package com.taxiapi.Repository;
 import com.taxiapi.Model.TaxiRoute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaxiRouteRepository extends JpaRepository<TaxiRoute,Long> {
+    List<TaxiRoute> findByfromLocationAndtoLocation(String fromLocation,
+                                                    String toLocation);
 }
