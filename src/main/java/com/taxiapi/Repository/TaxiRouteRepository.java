d551d5bb3de9ac9ef72baf6eab2c237ad905d5e1
Mokgadi Phasha <phashamokgadi631@gmail.com>
2025-04-14 15:36:51 +0200

    implemented logic for retrieving data from databse into a graph

--- src/main/java/com/taxiapi/Repository/TaxiRouteRepository.java
+++ src/main/java/com/taxiapi/Repository/TaxiRouteRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface TaxiRouteRepository extends JpaRepository<TaxiRoute,Long> {
     List<TaxiRoute> findByFromLocationAndToLocation(String fromLocation,
                                                     String toLocation);
+
+    List<TaxiRoute> findByFromLocation(String fromLocation);
 }
--- src/main/java/com/taxiapi/Service/Utility/ServiceUtility.java
+++ src/main/java/com/taxiapi/Service/Utility/ServiceUtility.java
@@ -1,9 +1,13 @@
 package com.taxiapi.Service.Utility;
 
 import com.taxiapi.Model.TaxiRoute;
+import com.taxiapi.Repository.TaxiRouteRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ServiceUtility {
@@ -12,6 +16,40 @@ public class ServiceUtility {
         return routes.stream()
                 .mapToDouble(TaxiRoute::getFare)
                 .sum();
+    }
+
+
+    public HashMap<String,List<String>> generateGraph(String fromLocation,
+                                                      TaxiRouteRepository db){
+        HashMap<String,List<String>> graph = new HashMap<>();
+
+        List<TaxiRoute> routes = getDatabaseRoutes(db,fromLocation);
+        List<String> neighbors = insertGraphNodes(routes);
+        graph.putIfAbsent(fromLocation,neighbors);
+
+
+        for (int i = 0; i < neighbors.size(); i++){
+            fromLocation = neighbors.get(i);
+            routes = getDatabaseRoutes(db,fromLocation);
+            List<String> new_neighbors = insertGraphNodes(routes);
+            graph.putIfAbsent(fromLocation,new_neighbors);
+            neighbors.addAll(new_neighbors);
+        }
+
+        return graph;
+
+    }
+
+
+    public List<String> insertGraphNodes(List<TaxiRoute> routes){
+        return routes.stream()
+                .map(TaxiRoute::getToLocation)
+                .toList();
+    }
+
+    public List<TaxiRoute> getDatabaseRoutes(TaxiRouteRepository db,
+                                             String fromLocation ){
+        return db.findByFromLocation(fromLocation);
     }
 
 
